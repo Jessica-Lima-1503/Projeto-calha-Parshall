29510e66446e8bb61133091f6d24e57dd4a23239
Nilson_Miguel97 <19juniormiguel@gmail.com>
2026-05-12 22:29:37 -0300

    Introduzndo Tabela 2

---
+++ src/Tabela2/CalculoTabela2.java
@@ -0,0 +1,23 @@
+package Tabela2;
+
+public class CalculoTabela2 {
+
+    public class CauculoTabela2 {
+        final static double[][] tabela = {
+                {1, 22.8,},
+                {3, 38.1},
+                {6, 45.7},
+                {9, 61.0},
+                {1, 91.5},
+                {1.5, 91.5},
+                {2, 91.5},
+                {3, 91.5},
+                {4, 91.5},
+                {4, 91.5},
+                {6, 91.5},
+                {7, 91.5},
+                {8, 91.5},
+                {10, 122.0}
+        };
+    }
+}
